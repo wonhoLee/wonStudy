773460e391a2c089fde776f35a9f5688ff7f48c3
wonho <lwonho@hotmail.com>
2018-09-20 16:09:34 +0900

    조건문을 다형성(polymorphism)으로 바꾸기

--- refactoring/src/ch1/refact/Movie.java
+++ refactoring/src/ch1/refact/Movie.java
@@ -6,21 +6,41 @@ public class Movie {
 	public static final int NEW_RELEASE = 1;
 	
 	private String _title;
-	private int _priceCode;
+	Price _price;
 	
 	public Movie(String _title, int _priceCode) {
 		super();
 		this._title = _title;
-		this._priceCode = _priceCode;
+		setPriceCode(_priceCode);
 	}
 	
 	public int getPriceCode() {
-		return _priceCode;
+		return _price.getPriceCode();
 	}
-	public void setPriceCode(int _priceCode) {
-		this._priceCode = _priceCode;
+	public void setPriceCode(int arg) {
+		switch (arg) {
+		case REGULAR:
+			_price = new RegularPrice();
+			break;
+		case CHILDREN:
+			_price = new ChildrenPrice();
+			break;
+		case NEW_RELEASE:
+			_price = new NewReleasePrice();
+			break;
+		default:
+			throw new IllegalArgumentException("Incorrect Price Code");
+		}
 	}
 	public String getTitle() {
 		return _title;
+	}
+
+	double getCharge(int daysRented) {
+		return _price.getCharge(daysRented);
+	}
+	
+	int getFrequentRenterPoints(int daysRented) {
+		return _price.getFrequentRenterPoints(daysRented)
 	}
 }
--- refactoring/src/ch1/refact/Rental.java
+++ refactoring/src/ch1/refact/Rental.java
@@ -1,7 +1,7 @@
 package ch1.refact;
 
 public class Rental {
-	private Movie _movie;
+	Movie _movie;
 	private int _daysRented;
 	
 	public Rental(Movie _movie, int _daysRented) {
@@ -17,37 +17,13 @@ public class Rental {
 	public int getDaysRented() {
 		return _daysRented;
 	}
-
-
+	
 	double getCharge() {
-		double result = 0;
-		//각 영화에 대한 요금 결정
-		switch (getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if(getDaysRented() > 2)
-				result += (getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += getDaysRented() * 3;
-			break;
-		case Movie.CHILDREN:
-			result += 1.5;
-			if (getDaysRented() > 3)
-				result += (getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return result;
-	}
-
-
-	int getFrequentRenterPoints() {
-		//최신(new realease)을 이틀 이상 대여하는 경우 추가포인트 제공
-		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-			return 2;
-		else
-			return 1;
+		return _movie.getCharge(_daysRented);
 	}
 	
+	double getFrequentRenterPoints() {
+		return _movie._price.getFrequentRenterPoints(_movie, _daysRented);
+	}
 	
 }
