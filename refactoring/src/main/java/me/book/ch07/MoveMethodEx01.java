c38055b7d5a49d8c47ed0d077fd9d2305a2235c9
wonho <lwonho@hotmail.com>
2018-10-10 17:36:14 +0900

    7장 객체간의 기능이동 - 진해중

---
+++ refactoring/src/main/java/me/book/ch07/MoveMethodEx01.java
@@ -0,0 +1,17 @@
+package me.book.ch07;
+
+public class MoveMethodEx01 {
+	
+	/*double overdraftCharge() {
+		if(_type.isPremium()) {
+			double result = 10;
+			if(_daysOverdrawn > 7) result += (_daysOverdrawn - 7) * 0.85;
+			return result;
+		}else
+			return _daysOverdrawn * 1.75;
+	}
+	
+	double bankCharge() {
+		
+	}*/
+}
