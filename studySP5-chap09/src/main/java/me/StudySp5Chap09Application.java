966da91cc116a38e1962c2453e3f12235b6f92e0
wonho <lwonho@hotmail.com>
2018-11-25 14:52:23 +0900

    [스터디] 스프링5 입문 - 스프링 MVC 시작 #12

---
+++ studySP5-chap09/src/main/java/me/StudySp5Chap09Application.java
@@ -0,0 +1,12 @@
+package me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySp5Chap09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySp5Chap09Application.class, args);
+	}
+}
