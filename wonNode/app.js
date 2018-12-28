42489427d1fa3d6549f7ce02fd2648072aef1f33
wonho <lwonho@hotmail.com>
2018-12-28 22:19:12 +0900

    image 연결 static 등록 controller 구현

--- wonNode/app.js
+++ wonNode/app.js
@@ -4,7 +4,13 @@ app.listen(3000, () => {
 	console.log("start!! express server on port 3000");
 });
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
 //	res.send("<h1>Hi friend!</>");
 	res.sendFile(__dirname + "/public/main.html");
 });
+
+app.get('/main', (req,res) => {
+	res.sendFile(__dirname + "/public/main.html");
+})
