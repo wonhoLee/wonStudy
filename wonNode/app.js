b805ed9c9fe4a11f542caac64f2c5c610108aae0
wonho <lwonho@hotmail.com>
2018-12-28 22:55:26 +0900

    body-parser 추가 post 처리

--- wonNode/app.js
+++ wonNode/app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
 app.listen(3000, () => {
 	console.log("start!! express server on port 3000");
 });
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
 //	res.send("<h1>Hi friend!</>");
@@ -13,4 +17,9 @@ app.get('/', (req, res) => {
 
 app.get('/main', (req,res) => {
 	res.sendFile(__dirname + "/public/main.html");
-})
+});
+
+app.post('/email_post', (req,res) => {
+	console.log(req.body.email);
+	res.send("<h1>welcome! " + req.body.email + "</h1>");
+});
