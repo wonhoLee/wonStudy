3c096b131cf905e7df74479d14911993ae5d630d
wonho <lwonho@hotmail.com>
2018-12-28 21:51:52 +0900

    HTML 기능 구현

--- wonNode/app.js
+++ wonNode/app.js
@@ -3,3 +3,8 @@ var app = express();
 app.listen(3000, () => {
 	console.log("start!! express server on port 3000");
 });
+
+app.get('/', (req, res) => {
+//	res.send("<h1>Hi friend!</>");
+	res.sendFile(__dirname + "/public/main.html");
+});
