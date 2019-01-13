59fe5103ecab6ec8a928d73a952bee32dee18a47
wonho <lwonho@hotmail.com>
2019-01-13 14:06:19 +0900

    JSON 활용한 Ajax처리

--- wonNode/app.js
+++ wonNode/app.js
@@ -1,11 +1,14 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
 
+var cors = require('cors');
+
+var app = express();
 app.listen(3000, () => {
 	console.log("start!! express server on port 3000");
 });
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -24,4 +27,11 @@ app.post('/email_post', (req,res) => {
 	console.log(req.body.email);
 //	res.send("<h1>welcome! " + req.body.email + "</h1>");
 	res.render('email.ejs', {'email' : req.body.email});
+});
+
+app.post('/ajax_send_email', (req,res) => {
+	console.log(req.body.email);
+	//check validation about input value => select db
+	var responseData = {'result' : 'ok', 'email' : req.body.email};
+	res.json(responseData);
 });
