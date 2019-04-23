1fef3310e1a62df6bf82bac3b57955fe79baa2a8
wonho <lwonho@hotmail.com>
2019-04-23 22:21:42 +0900

    CH8 Done

--- studyTDD/src/main/java/main/dollar/Franc.java
+++ studyTDD/src/main/java/main/dollar/Franc.java
@@ -4,7 +4,7 @@ public class Franc extends Money{
 	public Franc(int amount){
 		this.amount = amount; 
 	}
-	public Franc times(int multiplier) {
+	public Money times(int multiplier) {
 		return new Franc(this.amount * multiplier);
 	}
 }
--- studyTDD/src/main/java/main/dollar/Money.java
+++ studyTDD/src/main/java/main/dollar/Money.java
@@ -1,7 +1,16 @@
 package main.dollar;
 
-public class Money {
+public abstract class Money {
 	protected int amount;
+	abstract Money times(int multiplier);
+	
+	static Money franc(int amount) {
+		return new Franc(amount);
+	}
+	
+	static Money dollar(int amount) {
+		return new Dollar(amount);
+	}
 	
 	public boolean equals(Object object) {
 		Money money = (Money) object;
