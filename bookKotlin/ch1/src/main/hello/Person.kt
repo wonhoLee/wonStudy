a717aea7187f23c6a3e304d96e69394dfd8ff4dd
wonho <lwonho@hotmail.com>
2020-04-14 23:42:43 +0900

    bookKotlin

---
+++ bookKotlin/ch1/src/main/hello/Person.kt
@@ -0,0 +1,9 @@
+package main.hello
+
+data class Person(val name: String, val age: Int? = null)
+
+fun main(args: Array<String>) {
+    val persons = listOf(Person("영희"), Person("철수", age = 29))
+    val oldest = persons.maxBy { it.age ?: 0 }
+    println("나이가 가장 많은 사람: $oldest")
+}
