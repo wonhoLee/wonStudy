08e3898b5b6f885aa2622fbd9ea520628be8a646
wonho <lwonho@hotmail.com>
2020-04-23 00:44:00 +0900

    study

--- bookKotlin/ch2/src/main/s211/Color.kt
+++ bookKotlin/ch2/src/main/s211/Color.kt
@@ -3,7 +3,8 @@ package main.s211
 enum class Color (
         val r: Int, val g: Int, val b: Int
 ){
-    RED(255,0,0), ORANGE(255,165,0);
+    RED(255,0,0), ORANGE(255,165,0),
+    YELLOW(255,255,0);
     fun rgb() = (r * 256 + g) * 256 + b
 }
 
@@ -11,4 +12,11 @@ fun getMnemonic(color: Color) =
         when(color){
             Color.RED -> "Richard"
             Color.ORANGE -> "Of"
+            Color.YELLOW -> "test"
+        }
+
+fun getWarmth(color:Color) =
+        when(color){
+            Color.RED, Color.ORANGE -> "warm"
+            Color.YELLOW -> "test"
         }
---
+++ bookKotlin/ch2/src/main/s211/When.kt
@@ -0,0 +1,21 @@
+package main.s211
+
+import main.s211.Color.*
+import java.lang.Exception
+
+class When {
+    fun getWarmth(color:Color) = when(color){
+        RED, ORANGE, YELLOW -> "warm"
+    }
+
+    fun mix(c1:Color, c2:Color) = when(setOf(c1, c2)){
+        setOf(RED, YELLOW) -> ORANGE
+        else -> throw Exception("Dirty color")
+    }
+
+    fun mixOptimized(c1:Color, c2: Color) = when{
+        (c1 == RED && c2 == YELLOW) -> ORANGE
+        else -> throw Exception("Dirty color")
+    }
+}
+
