adc2f3b56ea42d80f7716aa23f9f6a3fd7b7d7be
wonho <lwonho@hotmail.com>
2020-04-24 23:42:30 +0900

    JPA -ch11-study

---
+++ bookJPA/bookJPACh11/src/main/java/main/BookJpaCh11Application.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookJpaCh11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookJpaCh11Application.class, args);
+	}
+
+}
