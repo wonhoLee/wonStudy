16984086bd73b3c3241979780bc827f0254caeef
wonho <lwonho@hotmail.com>
2020-05-13 23:16:06 +0900

    study

--- bookKotlin/ch2/src/main/s24/FizzBuzz.kt
+++ bookKotlin/ch2/src/main/s24/FizzBuzz.kt
@@ -1,8 +1,21 @@
 package main.s24
 
-fun fizzBuzz(i:Int) = when {
+import java.io.BufferedReader
+
+fun fizzBuzz(i: Int) = when {
     i % 15 == 0 -> "FizzBUzz"
     i % 3 == 0 -> "Fizz"
     i % 5 == 0 -> "Buzz"
     else -> "$i"
+}
+
+fun readNumber(reader: BufferedReader): Int? {
+    return try {
+        val line = reader.readLine()
+        Integer.parseInt(line)
+    } catch (e: NumberFormatException) {
+        null
+    } finally {
+        reader.close()
+    }
 }
