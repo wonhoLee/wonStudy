63785a4f1935514f40edb2a8c5352d72777d544f
wonho <lwonho@hotmail.com>
2020-09-20 10:55:24 +0900

    transactional study init

---
+++ studyTransactional/src/main/java/home/study/studyTransactional/StudyTransactionalApplication.java
@@ -0,0 +1,13 @@
+package home.study.studyTransactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTransactionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTransactionalApplication.class, args);
+	}
+
+}
---
+++ studyTransactional/src/test/java/home/study/studyTransactional/StudyTransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package home.study.studyTransactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTransactionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
